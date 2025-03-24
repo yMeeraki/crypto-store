fe626e748a5733f56574f6963db4d43fccb99e63
Yukti Gupta <yuktigupta95@gmail.com>
2025-03-24 19:26:30 +0530

    space to navitem in small screen

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const menuToggle = document.getElementById("menuToggle");
-const navMenu = document.getElementById("navMenu");
 
-menuToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-});
